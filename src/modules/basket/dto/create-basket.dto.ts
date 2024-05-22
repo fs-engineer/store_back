76943ef97d3f12d3d19895b0493d977d6ad4ee35
Vadym Yevlanov <vad.evlanov@gmail.com>
2024-05-22 14:01:55 +0300

    added submit, cancel buttons, added modal window

--- src/modules/basket/dto/create-basket.dto.ts
+++ src/modules/basket/dto/create-basket.dto.ts
@@ -2,29 +2,29 @@ import { IsNotEmpty, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBasketDto {
-  @ApiProperty({
-    example: '123',
-    description: 'User id, for auth users',
-  })
-  userId?: number;
+    @ApiProperty({
+        example: '123',
+        description: 'User id, for Auth users',
+    })
+    userId?: number;
 
-  @ApiProperty({ example: '123', description: 'Product id', required: true })
-  @IsNotEmpty()
-  @IsNumber()
-  productId: number;
+    @ApiProperty({ example: '123', description: 'Product id', required: true })
+    @IsNotEmpty()
+    @IsNumber()
+    productId: number;
 
-  @ApiProperty({
-    example: '12',
-    description: 'Total number of products',
-    required: true,
-  })
-  @IsNotEmpty()
-  @IsNumber()
-  quantity: number;
+    @ApiProperty({
+        example: '12',
+        description: 'Total number of products',
+        required: true,
+    })
+    @IsNotEmpty()
+    @IsNumber()
+    quantity: number;
 
-  @ApiProperty({
-    example: 'ksdf97352_238fdshf',
-    description: 'Session Id, for guests',
-  })
-  sessionId?: string;
+    @ApiProperty({
+        example: 'ksdf97352_238fdshf',
+        description: 'Session Id, for guests',
+    })
+    sessionId?: string;
 }
